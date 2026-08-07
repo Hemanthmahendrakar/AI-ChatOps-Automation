8bda6bc8d08b35a5ae633d8a5a9e23d83d8cf2a7
Hemanth Kumar Rao M <mahendrakarhemanth@gmail.com>
2026-08-07 11:07:21 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ai-infrastructure-terraform-state"
+    bucket = "hk-ai-infrastructure-terraform-state"
     key    = "terraform/state.tfstate"
     region = "us-east-1"
   }
