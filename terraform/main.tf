35922121be5e2bbf5085545a36153c2948836e8f
Hemanth Kumar Rao M <mahendrakarhemanth@gmail.com>
2026-08-07 11:40:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,31 +151,12 @@ resource "aws_security_group" "ai_servers" {
 }
 
 
-resource "tls_private_key" "ssh_key" {
-
-  algorithm = "RSA"
-
-  rsa_bits = 4096
-
-}
-
 
 resource "aws_key_pair" "ai_key" {
 
   key_name = "${var.project_name}-key"
 
   public_key = tls_private_key.ssh_key.public_key_openssh
-
-}
-
-
-resource "local_file" "private_key" {
-
-  content = tls_private_key.ssh_key.private_key_pem
-
-  filename = "${path.module}/key/ai-server-key.pem"
-
-  file_permission = "0400"
 
 }
 
